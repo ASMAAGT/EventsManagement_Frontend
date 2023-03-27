16a4af45833d10c20ac82ee243e007370fd09933
ASMAAGT <asmaguetari1@gmail.com>
2023-03-27 14:05:43 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './View/Authentification/login/login.component';
+import { ErrorPageComponent } from './View/error-page/error-page.component';
+import { HomeComponent } from './View/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/home',pathMatch:'full'},
+  {path:'/home', component: HomeComponent},
+  {path:'/login', component: LoginComponent},
+  {path: '**',component:ErrorPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './View/Authentification/login/login.component';
+import { AuthentificationComponent } from './View/Authentification/authentification.component';
+import { RegisterComponent } from './View/Authentification/register/register.component';
+import { HomeComponent } from './View/home/home.component';
+import { ErrorPageComponent } from './View/error-page/error-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AuthentificationComponent,
+    RegisterComponent,
+    HomeComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
